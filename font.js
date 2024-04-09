e0e72eb6b553cc2ba23c56b49902202ab8b40db3
achoj <375017287@qq.com>
2024-04-09 10:50:03 +0800

    add

--- font.js
+++ font.js
@@ -1,16 +1,63 @@
-var Fontmin = require("fontmin")
+var Fontmin = require("fontmin");
+var fs = require("fs");
+var path = require("path");
 
-var fontmin = new Fontmin()
-  .src("./fonts/SiYuan.ttf")
-  .dest("./fontmins/")
-  .use(
-    Fontmin.glyph({
-      text: "从而自定义自己博客园markdown样式.当然本文也可以当markdown语法学习之用.  "
-    })
-  )
+// 指定目录
+var directoryPath = "./articles";
+// 存储所有文件内容的变量
+var allContent = "";
 
-fontmin.run(function(err, files) {
-  if (err) {
-    throw err
-  }
-})
+// 递归读取目录下的文件
+function readFilesInDirectory(directoryPath) {
+  fs.readdir(directoryPath, function(err, files) {
+    if (err) {
+      console.error("Error reading directory:", err);
+      return;
+    }
+    files.forEach(function(file) {
+      var filePath = path.join(directoryPath, file);
+      fs.stat(filePath, function(err, stat) {
+        if (err) {
+          console.error("Error reading file:", err);
+          return;
+        }
+        if (stat.isDirectory()) {
+          // 如果是目录，则递归读取目录下的文件
+          readFilesInDirectory(filePath);
+        } else {
+          // 如果是文件，则读取文件内容并追加到 allContent 变量中
+          fs.readFile(filePath, "utf8", function(err, data) {
+            if (err) {
+              console.error("Error reading file:", err);
+              return;
+            }
+            allContent += data; // 追加文件内容
+          });
+        }
+      });
+    });
+  });
+}
+
+// 在所有文件读取完成后执行 Fontmin 处理
+function processAllContent() {
+  console.log(allContent)
+  var fontmin = new Fontmin()
+    .src("./fonts/SiYuan.ttf")
+    .dest("./fontmins/")
+    .use(Fontmin.glyph({ text: allContent }));
+
+  fontmin.run(function(err, files) {
+    if (err) {
+      console.error("Error processing content:", err);
+      return;
+    }
+    console.log("Fontmin processed", files.length, "files successfully.");
+  });
+}
+
+// 从指定目录开始递归读取文件
+readFilesInDirectory(directoryPath);
+
+// 当所有文件读取完成后，执行 Fontmin 处理
+setTimeout(processAllContent, 1000); // 适当增加延迟以确保所有文件都被读取完毕
